8a4990a9ae9137cd029ca71bcb689f036f02b410
Ian Chen <iancodinghtml@gmail.com>
2023-01-11 20:47:47 +0800

    typofix: AMF -> SMF

--- factory/config.go
+++ factory/config.go
@@ -5,7 +5,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * AMF Configuration Factory
+ * SMF Configuration Factory
  */
 
 package factory
--- factory/factory.go
+++ factory/factory.go
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 /*
- * AMF Configuration Factory
+ * SMF Configuration Factory
  */
 
 package factory
