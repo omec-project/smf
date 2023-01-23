1d8730dc8e7c3ce383fa8d89c5dc86608e42a2cc
Amol Jaikar <111894366+amolonf@users.noreply.github.com>
2023-01-23 01:26:32 -0500

    Update api_nf_subscribe_notify.go
    Added the license information

--- callback/api_nf_subscribe_notify.go
+++ callback/api_nf_subscribe_notify.go
@@ -1,3 +1,11 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+// SPDX-FileCopyrightText: 2021 Open Networking Foundation <info@opennetworking.org>
+// Copyright 2019 free5GC.org
+//
+// SPDX-License-Identifier: Apache-2.0
+
+
+
 package callback
 
 import (
