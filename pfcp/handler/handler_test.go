99cb324bd4e24945ee60ff33f280e7d0c161bde7
Guillaume Belanger <guillaume.belanger27@gmail.com>
2024-07-09 13:59:48 -0400

    chore: improve unit test
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- pfcp/handler/handler_test.go
+++ pfcp/handler/handler_test.go
@@ -7,8 +7,10 @@ package handler_test
 import (
 	"net"
 	"testing"
+	"time"
 
 	"github.com/omec-project/smf/context"
+	"github.com/omec-project/smf/factory"
 	"github.com/omec-project/smf/pfcp/handler"
 	pfcp_message "github.com/omec-project/smf/pfcp/message"
 	"github.com/wmnsk/go-pfcp/ie"
@@ -24,6 +26,10 @@ func (f *Flag) SetBit(position uint8) {
 		return
 	}
 	*f |= 1 << (position - 1)
+}
+
+func BoolPointer(b bool) *bool {
+	return &b
 }
 
 func TestFindUEIPAddressNoAddressInCreatedPDR(t *testing.T) {
@@ -72,14 +78,35 @@ func TestFindUEIPAddressNoUEIPAddressInCreatedPDR(t *testing.T) {
 }
 
 func TestHandlePfcpAssociationSetupResponse(t *testing.T) {
+	kafkaInfo := factory.KafkaInfo{
+		EnableKafka: BoolPointer(false),
+	}
+	configuration := &factory.Configuration{
+		KafkaInfo:        kafkaInfo,
+		EnableUpfAdapter: false,
+	}
+	factory.SmfConfig = factory.Config{
+		Configuration: configuration,
+	}
 	upNodeID := context.NewNodeID("1.1.1.1")
-	context.NewUPF(&upNodeID, nil)
+	upf := context.NewUPF(&upNodeID, nil)
+	SnssaiInfos := make([]context.SnssaiUPFInfo, 0)
+	snssaiInfo := context.SnssaiUPFInfo{
+		DnnList: []context.DnnUPFInfoItem{
+			{
+				Dnn: "internet",
+			},
+		},
+	}
+	SnssaiInfos = append(SnssaiInfos, snssaiInfo)
+	upf.SNssaiInfos = SnssaiInfos
 	pfcp_message.InsertPfcpTxn(1, &upNodeID)
 	msg := message.NewAssociationSetupResponse(
 		1,
 		ie.NewCause(ie.CauseRequestAccepted),
 		ie.NewNodeID("1.1.1.1", "", ""),
-		ie.NewUserPlaneIPResourceInformation(uint8(0x61), 0, "1.2.3.4", "", "", ie.SrcInterfaceAccess),
+		ie.NewUserPlaneIPResourceInformation(uint8(0x61), 0, "1.2.3.4", "", "internet", ie.SrcInterfaceAccess),
+		ie.NewRecoveryTimeStamp(time.Now()),
 	)
 
 	remoteAddress := &net.UDPAddr{
@@ -88,5 +115,4 @@ func TestHandlePfcpAssociationSetupResponse(t *testing.T) {
 	}
 
 	handler.HandlePfcpAssociationSetupResponse(msg, remoteAddress)
-	// panic("TestHandlePfcpAssociationSetupResponse")
 }
